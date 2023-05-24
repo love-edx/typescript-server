501beb9c4c3df8b2ac8043f8f31adf36859fa058
love-edx <l.tyagi@edexa.team>
2023-05-24 15:06:21 +0530

    feat: test case issue fixed

--- tests/index.ts
+++ tests/index.ts
@@ -2,6 +2,7 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import { helloWorld } from "../server";
+import { exit } from "process";
 
 chai.use(chaiHttp);
 
@@ -9,10 +10,15 @@ describe('', function () {
     it('It should return hello-world', function (done) {
         const indexFunction = helloWorld();
         indexFunction.then(data => {
-            expect(data)
+            expect(data).to.be.an('object').with.all.keys('status', 'message');
+            expect(data.status).to.be.an('number');
+            expect(data.message).to.be.an('string');
             done()
         }).catch(error => {
             done(new Error(error.message));
+        });
+        after(() => {
+            exit()
         })
     });
 })
