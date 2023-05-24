b718dfd978b6db8d52cde292fd1be20dbee6758e
love-edx <l.tyagi@edexa.team>
2023-05-24 14:31:31 +0530

    feat: addition of ts file

--- tests/index.ts
+++ tests/index.ts
@@ -9,9 +9,7 @@ describe('', function () {
     it('It should return hello-world', function (done) {
         const indexFunction = helloWorld();
         indexFunction.then(data => {
-            expect(data).to.be.an('object').with.all.keys('status', 'message');
-            expect(data.status).to.be.an('number');
-            expect(data.message).to.be.an('string');
+            expect(data)
             done()
         }).catch(error => {
             done(new Error(error.message));
