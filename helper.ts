b10848ddfccd7674daa71d05ec72d494cf9c413a
love-edx <l.tyagi@edexa.team>
2023-06-08 11:50:26 +0530

    feat: cron jobs testing

---
+++ helper.ts
@@ -0,0 +1,35 @@
+import { CronJob } from "cron";
+let cronJobs = [{ time: 1, isActive: 0 }, { time: 2, isActive: 0 }, { time: 3, isActive: 0 }];
+let tempCronJobs = [];
+export async function jobScheduler(data: any) {
+    const jobTime = await cronTimeHelper(cronJobs)
+    new CronJob(jobTime, () => {
+        console.info('Webhook cronjob is running');
+    }).start();
+    return jobTime;
+}
+
+async function cronTimeHelper(timeData: any) {
+    let cronTimer;
+    for (let i = 0; i <= timeData.length; i++) {
+        if (timeData[i].isActive === 0) {
+            // at each cron job update running cron job timer as true
+            if (timeData[i].time == 5 || timeData[i].time == 30) cronTimer = `*/${timeData[i].time} * * * * *`;
+            else if (timeData[i].time == 1 || timeData[i].time == 6) cronTimer = `0 ${timeData[i].time} * * * `;
+            else cronTimer = `0 0 * * * `;
+
+            // if last element is going to iterate so set all isActive as 0
+            const lastItem = timeData[timeData.length - 1];
+            if (timeData[i].time === lastItem.time) {
+                cronJobs = tempCronJobs;
+                cronJobs.push(timeData[i]);
+                tempCronJobs = [];
+                break;
+            } else {
+                tempCronJobs.push(cronJobs.splice(i, 1)[0]);
+                break;
+            }
+        }
+    }
+    return cronTimer;
+}
--- server.ts
+++ server.ts
@@ -4,26 +4,45 @@ import { Bstamp, Network } from 'stamping'
 const PORT = process.env.PORT || 4000;
 const stamping = new Bstamp();
 import { CronJob } from "cron";
+import { jobScheduler } from "./helper";
+const data = {
+    // 'client-id': "b1451bc9-4d8a-4e51-838c-c2341a1c13c3",
+    // 'secret-key': "F7D866D57ACA8071817D28A49C81CDDEE74899492B11C2FE3FE9818368956DC91150C138AC46770B273FE8E7665C2D41DE1A11A728D318CB86BC4627C72FA58A",
+}
 
+// Cron job that will run after each 5 minute
+new CronJob('*/5 * * * *', () => {
+    console.log('Cron job running every 5 minutes...');
+});
 
-// Cron job that wll run after 2 minutes.
-new CronJob('*/1,5,30,59 */1 * * *', () => {
-    const date = new Date()
-    console.info('Cron Job minute based ', date.toLocaleDateString(), date.getHours(), date.getMinutes(), date.getSeconds());
+// Cron job that will run after each 30 minute
+new CronJob('*/30 * * * *', () => {
+    console.log('Cron job running every 30 minutes...');
+});
+// Cron job that will run after each 1 hours
+new CronJob('0 */1 * * *', () => {
+    jobScheduler(data);
+    console.log(' ------------- Cron job execute in 1 hour -------');
 }).start();
 
+// Cron job that will run after each 6 hours
+new CronJob('0 * /6 * * *', () => {
+    console.log(' ------------- Cron job once in 6 hours -------');
+    jobScheduler(data)
+}).start();
 
 // Cron job that will run once a day
-new CronJob('* */6 * * *', () => {
-    const date = new Date()
-    console.info(' ------------- Cron Job hours based -------', date.toLocaleDateString(), date.getHours(), date.getMinutes(), date.getSeconds());
+new CronJob('0 0 0 * * *', () => {
+    jobScheduler(data)
+    console.log(' ------------- Cron job at each day -------');
 }).start();
 
-// Cron job that will run once a day
-new CronJob('0 0 * * *', () => {
-    const date = new Date()
-    console.info(' ------------- Cron Job day basis -------', date.toLocaleDateString(), date.getHours(), date.getMinutes(), date.getSeconds());
-}).start();
+
+// // Cron job that will run once a day
+// new CronJob('0 0 * * *', () => {
+//     const date = new Date()
+//     console.info(' ------------- Cron Job day basis -------', date.toLocaleDateString(), date.getHours(), date.getMinutes(), date.getSeconds());
+// }).start();
 
 const authSettings = {
     headers: {
@@ -36,13 +55,16 @@ async function helloWorld() {
 }
 
 app.get('/', async (req, res) => {
-    await stamping.authenticate(authSettings)
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((error) => {
-            res.send(error);
-        });
+    const schedulerData = jobScheduler(authSettings);
+    return res.status(200).json({ message: 'Cron job function is working fine', data: schedulerData });
+
+    // await stamping.authenticate(authSettings)
+    //     .then((data) => {
+    //         res.send(data);
+    //     })
+    //     .catch((error) => {
+    //         res.send(error);
+    //     });
 })
 
 app.listen(PORT, () =>
